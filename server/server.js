f0443977e425c017e87f9a94019ed76c0bfe4235
ghigbie <gehigbie@gmail.com>
2021-10-19 19:34:26 -0400

    Edit typo

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express()
-const PORT = process.evn.port || 8000;
+const PORT = process.env.port || 8000;
 
 fs.readdirSync('./routes').map( route => app.use('/api', require(`./routes/${route}`)));
+
 
 
 app.listen(PORT,  () => console.log(`Server is up on ${PORT}`));
